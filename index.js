853601d93d851b97ad47f14a44bdf9d84564403f
Subham0629 <112814727+Subham0629@users.noreply.github.com>
2023-03-26 02:50:37 +0530

    2ndattempted

--- index.js
+++ index.js
@@ -7,4 +7,26 @@ function closemenu(){
     sidemenu.style.right="-200px";
 }
 
- 
+var typed = new Typed(".typing", {
+  strings: ["Full Stack Web Developer"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
+})
+
+let resume1 = document
+  .getElementById("resume-link-1")
+   .addEventListener("click",opendrive);
+
+  let resume2 = document
+  .getElementById("resume-link-2")
+  .addEventListener("click",opendrive);
+
+
+function opendrive(){
+  window.open(
+    (href =
+      "https://drive.google.com/file/d/1B3Tdzax-cE-dd75tsRg2-gUTBD2c5wpN/view?usp=share_link")
+  );
+ }
+
